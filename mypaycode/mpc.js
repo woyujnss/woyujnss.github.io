ae463968845fb721972aadc2b6d6040e85f620cc
vbox007 <vbox007@sina.com>
2025-06-05 02:15:48 +0800

    把跳转方式从replace改为href，并增加了一个QQ的收款码

--- mypaycode/mpc.js
+++ mypaycode/mpc.js
@@ -22,22 +22,28 @@
     const isAlipayFeature = function () {
         return typeof ap !== 'undefined' && ap.config;
     }
-    const detectBrowserComprehensive = function (wcFn, alFn, otFn) {
+    const detectBrowserComprehensive = function (wcFn, alFn, qqFn, otFn) {
         if (isWeChat() || isWeChatObj() || isWeChatFeature()) {
             wcFn();
         } else if (isAlipay() || isAlipayObj() || isAlipayFeature()) {
             alFn();
+        } else if (navigator.userAgent.toLowerCase().indexOf('qq/')) {
+            qqFn();
         } else {
             otFn();
         }
     }
-    document.write(navigator.userAgent.toLowerCase());
     detectBrowserComprehensive(() => {
-        location.replace('https://payapp.wechatpay.cn/sjt/qr/AQFekM36zmmtZocQSXpRh2PR');
+        location.href = 'https://payapp.wechatpay.cn/sjt/qr/AQFekM36zmmtZocQSXpRh2PR';
+        // location.replace('https://payapp.wechatpay.cn/sjt/qr/AQFekM36zmmtZocQSXpRh2PR');
         // location.replace('wxp://f2f0Sc8TdE9JeO-zg0ZTvjTN9EfYVrlEp6lCzFlb2Q9gq1vL2Gpfsg-moHzQ0nbSGK0y');
     }, () => {
-        location.replace('https://qr.alipay.com/tsx18738qu1ut53gqafni8f');
+        location.href = 'https://qr.alipay.com/tsx18738qu1ut53gqafni8f';
+        // location.replace('https://qr.alipay.com/tsx18738qu1ut53gqafni8f');
     }, () => {
-        document.write('\n我艹？？？');
+        location.href = 'https://i.qianbao.qq.com/wallet/sqrcode.htm?m=tenpay&f=wallet&a=1&u=479434&n=J-thirteen&ac=CAEQyqEdGP2VgsIGMhjmtYHliqjmkYrkvY3nu4_okKXmlLbmrL44AUIgYTRjYWZjNjJiOTNkOTNjYWVlZmE1ZjU3YWEzMDI4ZmU%3D_xxx_sign';
+    }, () => {
+        document.write('我艹？？？\n');
     });
+    document.write(navigator.userAgent);
 }
